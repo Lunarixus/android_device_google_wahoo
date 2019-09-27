7644141ac6397f9f6817c63f3a3e28a67b49e4c4
Josh Chasky <jchasky@gmail.com>
2019-09-27 12:55:41 -0400

    Ensure the proper Google Assistant prop is inherited
    * Also add ANGLE prop
    
    Change-Id: Idb6962d140d05b0ea8180e56e2eccb1e0ba48060

--- device.mk
+++ device.mk
@@ -727,3 +727,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Use /product/etc/fstab.postinstall to mount system_other.
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.postinstall.fstab.prefix=/product
+
+# Enable missing props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.opa.eligible_device=true \
+    ro.gfx.angle.supported=true
